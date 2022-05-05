5fc0a5d19d56890305212514ddf769b8557b36ea
Garaje de Ideas <82031166+GarajedeIdeas@users.noreply.github.com>
2022-05-05 18:20:04 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/register/register.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/main' },
+  {
+    path: 'main',
+    component: MainComponent,
+    ...canActivate(() => redirectUnauthorizedTo(['/register']))
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  formReg: FormGroup;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.formReg = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.userService.register(this.formReg.value)
+      .then(response => {
+        console.log(response);
+        this.router.navigate(['/login']);
+      })
+      .catch(error => console.log(error));
+  }
+
+}
